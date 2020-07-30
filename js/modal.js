bc8925e1416df542f057f1da75cc0e672201c102
galka-travinskaya <galka03031988@rambler.ru>
2020-07-30 18:48:48 +0300

    Добавляет адаптивку

--- js/modal.js
+++ js/modal.js
@@ -9,22 +9,9 @@
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
+      document.body.classList.toggle('modal-open')
       refs.modal.classList.toggle('is-hidden');
     }
   })();
-
-  
-  /*(() => {
-    document.querySelector('.js-form').addEventListener('submit', e => {
-      e.preventDefault();
-
-      console.log('');
-      console.count('Submit #');
-
-      new FormData(e.currentTarget).forEach((value, name) =>
-        console.log(`${name}: ${value}`),
-      );
-    });
-  })();*/
     
     
